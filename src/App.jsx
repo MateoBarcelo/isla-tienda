648b91cfc771dbad0b0b30c3afe0eb887e2de627
MateoBarcelo <mateobarcelo88@gmail.com>
2023-10-09 21:18:21 -0300

    Init repo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { CartIcon } from './components/Icons'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='text-4xl text-center'>Shopping Cart</h1>
+    <CartIcon></CartIcon>
+   </>
+  )
+}
+
+export default App
