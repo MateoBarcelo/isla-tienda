ef7227a130bc87cfd966d08aea686ce770af391a
MateoBarcelo <mateobarcelo88@gmail.com>
2023-11-29 00:57:56 -0300

    Added dashboard order to user UserButton

--- src/components/Header/UserButton.jsx
+++ src/components/Header/UserButton.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import { UserIcon } from "../Icons"
 import { useAuth } from "../../context/auth"
+import { useIsAdmin } from "../../hooks/useIsAdmin"
 
 const UserButton = () => {
     const [open, setOpen] = useState(false)
     const { user, signout } = useAuth()
+
+    const {admin} = useIsAdmin()
 
     return(
         <div class="relative inline-block text-left">
@@ -19,10 +22,9 @@ const UserButton = () => {
                     <a href="#" class="text-mint-900 active:bg-gray-100 hover:bg-gray-50 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Sesión como</a>
                     <a class="text-mint-900 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">{user}</a>
                 </div>
-                <div className="py-1" role="none">
-                    <a href="#" className="text-mint-900 active:bg-gray-100 hover:bg-gray-50 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-4">Share</a>
-                    <a href="#" className="text-mint-900 active:bg-gray-100 hover:bg-gray-50 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-5">Add to favorites</a>
-                </div>
+                {admin && <div className="py-1" role="none">
+                    <a href="/orderdashboard" className="text-mint-900 active:bg-gray-100 hover:bg-gray-50 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-4">Panel de órdenes</a>
+                </div>}
                 <div className="py-1" role="none" onClick={signout}>
                     <a href="/" className="text-mint-900 active:bg-gray-100 hover:bg-gray-50 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-6">Salir</a>
                 </div>
