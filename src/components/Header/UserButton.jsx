25be6786e96c2e8d248eaceb26e031cb776a096f
Legadyn <legadyn@gmail.com>
2023-12-10 23:40:58 -0300

    fixed: user mobile menu

--- src/components/Header/UserButton.jsx
+++ src/components/Header/UserButton.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { UserIcon } from "../Icons"
 import { useAuth } from "../../context/auth"
 import { useIsAdmin } from "../../hooks/useIsAdmin"
