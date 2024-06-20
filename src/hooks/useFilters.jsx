6766640b1262070d3901cbb1c8e4db0a448550d5
MateoBarcelo <mateobarcelo88@gmail.com>
2024-06-19 22:18:24 -0300

    fix: conditional on filters not working properly

--- src/hooks/useFilters.jsx
+++ src/hooks/useFilters.jsx
@@ -3,9 +3,10 @@ import { FiltersContext } from "../context/filters"
 
 export function useFilters() {
     const {filters, setFilters} = useContext(FiltersContext)
+    
     const filterProducts = (products) => { 
         return products.filter(product => {
-            return product.price <= filters.maxPrice && (filters.category === 'all' || product.category === filters.category)
+            return Number.parseFloat(product.price) <= filters.maxPrice && (filters.category === 'all' || product.category === filters.category)
       })
     }
   
